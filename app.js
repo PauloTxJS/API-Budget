d8bc5e123e7fa06763c8b22266023f3c115e6b87
Paulo Teixeira <paulo.teixeira541@gmail.com>
2021-03-08 12:12:55 -0300

    I changed the database name

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
     next();
 });
 
-mongoose.connect('mongodb://localhost:27017/paulo', {
+mongoose.connect('mongodb://localhost:27017/budget', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
